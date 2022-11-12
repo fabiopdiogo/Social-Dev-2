04bd65b9b4625194615ceb344633bac0cc00ddeb
Fabio Diogo <fabindiogo@gmail.com>
2022-11-12 12:02:27 -0300

    Criado middleware para conexão com o BD

--- pages/api/user/signup.js
+++ pages/api/user/signup.js
@@ -1,6 +1,6 @@
-import connect from 'next-connect'
-
 import Joi from 'joi'
+
+import createHandler from '../../../lib/midddlewares/readConnect'
 
 import validate from '../../../lib/midddlewares/validation'
 
@@ -14,8 +14,9 @@ const postSchema = Joi.object({
   password: Joi.string().required().max(50).min(6),
 })
 
-const signup =  connect()
-  .post(validate({ body: postSchema }),(req, res) =>{
+const signup =  createHandler()
+
+signup.post(validate({ body: postSchema }),(req, res) =>{
     signupUser(req.body)
     res.status(200).json({ teste: "ok"})
   })
