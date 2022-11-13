d2f3b904f16479759e3fda3452d7766ed8ce735f
Fabio Diogo <fabindiogo@gmail.com>
2022-11-13 14:57:15 -0300

    integrado o cadastro com o front e back end

--- pages/signup.jsx
+++ pages/signup.jsx
@@ -1,8 +1,11 @@
-import { useState } from 'react'
 import styled from 'styled-components'
 import Link from 'next/link'
 import { useForm } from 'react-hook-form'
 import { joiResolver } from '@hookform/resolvers/joi'
+
+import axios from 'axios'
+import { useRouter } from 'next/router'
+
 
 import { signupSchema } from '../modules/user/user.schema'
 
@@ -27,15 +30,25 @@ const Text =styled.p`
 `
 
 function SignupPage () {
-  const {control, handleSubmit, formState: { errors }} = useForm({
+  const router = useRouter()
+  const {control, handleSubmit, formState: { errors }, setError} = useForm({
     resolver: joiResolver(signupSchema)
   })
 
-  const handleForm = (data) =>{
-    console.log(data)
+  const handleForm = async (data) =>{
+    try{
+      const { status } = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/api/user/signup`, data)
+      if (status === 201){
+        router.push('/')
+      }
+    }catch (err){
+      if (err.response.data.code === 11000){
+        setError(err.response.data.duplicatedKey, {
+          type: 'duplicated'
+        })
+      }
+    }
   }
-  
-  console.log(errors)
 
   return(
     <ImageWithSpace>
@@ -43,7 +56,7 @@ function SignupPage () {
       <H4>Tudo o que acontece no mundo dev, está aqui!</H4>
       <FormContainer>
         <H2>Crie sua conta</H2>
-        <Form onSubmit={handleSubmit(handleForm)} control={control}>
+        <Form onSubmit={handleSubmit(handleForm)}>
           <Input label="Nome" name="firstName" control={control}/>
           <Input label="Sobrenome" name="lastName" control={control}/>
           <Input label="Usuário" name="user" control={control}/>
