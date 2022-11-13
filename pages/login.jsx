df328e4460ad83ba89807fdf18cf10809e235941
Fabio Diogo <fabindiogo@gmail.com>
2022-11-13 16:08:37 -0300

    Sistema de login feito

--- pages/login.jsx
+++ pages/login.jsx
@@ -1,6 +1,12 @@
 import styled from "styled-components"
 import ImageWithSpace from "../src/components/layout/ImageWithSpace"
 import Link from 'next/link'
+import { joiResolver } from '@hookform/resolvers/joi'
+import axios from 'axios'
+import { useRouter } from 'next/router'
+import { useForm } from 'react-hook-form'
+
+import { loginSchema } from '../modules/user/user.schema'
 
 import H1 from "../src/components/typography/H1"
 import H2 from "../src/components/typography/H2"
@@ -31,16 +37,41 @@ const Text =styled.p`
 `
 
 function Login(){
+  const router = useRouter()
+  const {control, handleSubmit, formState: { errors }, setError} = useForm({
+    resolver: joiResolver(loginSchema)
+  })
+
+  const onSubmit = async (data) => {
+    try{
+      const { status } = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/api/user/login`, data)
+      if (status === 200){
+        router.push('/')
+      }
+    } catch ({response}) {
+      if(response.data === 'password incorrect'){
+        setError('password',{
+          message: 'A senha está incorreta.'
+        })
+      }
+      else if (response.data === 'not found'){
+        setError('userOrEmail',{
+          message: 'Usuário ou e-mail não encontrado.'
+        })
+      }
+      
+    }
+  }
   return(
     <ImageWithSpace>     
       <Title>#Social Dev</Title>
       <Subtitle>Tudo o que acontece no mundo dev, está aqui!</Subtitle>
       <FormContainer>
         <H2>Entre em sua conta</H2>
-        <Form>
-          <Input label="Email ou usuário"/>
-          <Input label="Senha"/>
-          <Button type="submit">Entrar</Button>
+        <Form onSubmit={handleSubmit(onSubmit)}>
+        <Input label="Email ou usuário" name="userOrEmail" control={control}/>
+          <Input label="Senha" type="password" name="password" control={control}/>
+          <Button type="submit" disabled={Object.keys(errors).length > 0}>Entrar</Button>
         </Form>
         <Text>Não possui uma conta? <Link href="/signup">Faça seu cadastro</Link></Text>
       </FormContainer>
